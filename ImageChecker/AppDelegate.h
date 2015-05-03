19e04062ca018c604217cb5d11ee77dabbe7241e
Neeraj Jhawar <neeraj@sols.com>
2015-05-02 20:32:19 -0400

    Initial Commit

---
+++ ImageChecker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageChecker
+//
+//  Created by Neeraj Jhawar on 5/2/15.
+//  Copyright (c) 2015 SolsSystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageChecker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ImageChecker
+//
+//  Created by Neeraj Jhawar on 5/2/15.
+//  Copyright (c) 2015 SolsSystems. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
